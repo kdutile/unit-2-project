89ef3a1911cc142b1b6d3eb04dab4f131580af61
Kevin Dutile <4323281+kdutile@users.noreply.github.com>
2022-02-17 23:32:46 -0500

    updated seed data

--- models/someMeals.js
+++ models/someMeals.js
@@ -2,19 +2,61 @@ module.exports = [
   {
     name: "Build Your Own Bowls",
     img: "https://i.imgur.com/vtMuD5g.jpg",
-    description: "Choose a grain, two sides, and a protein.",
+    description: "Choice of grain, two sides, and a protein",
     allergies: ["Vegetarian"]
+  },
+  {
+    name: "Chicken Parmesan",
+    img: "https://i.imgur.com/6yQ6v3j.jpg?1",
+    description: "Hand breaded chicken, san Marzano tomato sauce, parmesan, provolone",
+    allergies: ["Gluten", "Dairy"]
+  },
+  {
+    name: "Wings",
+    img: "https://i.imgur.com/ELSSqeI.jpg?1",
+    description: "Plain, Carolina Mop Sauce, and Honey Sriracha",
+    allergies: ["Gluten"]
+  },
+  {
+    name: "Fried Chicken Sandwiches",
+    img: "https://i.imgur.com/OnHUrhg.jpg",
+    description: "Hand breaded and fried chicken, bacon, house pickles, and green goddess sauce",
+    allergies: ["Gluten", "Dairy"]
   },
   {
     name: "Red Sauce Night",
     img: "https://i.imgur.com/pFX3o1w.jpg",
-    description: "Chicken parmesian or meatballs over pasta in red sauce.",
+    description: "Choice of chicken parmesan or meatballs, over pasta in red sauce",
     allergies: ["Gluten", "Dairy"]
   },
   {
+    name: "Taco Tuesday",
+    img: "https://i.imgur.com/UxGo0wu.jpg",
+    description: "Title says it all",
+    allergies: []
+  },
+  {
+    name: "Buttermilk Mashed Potatoes",
+    img: "https://i.imgur.com/qO6lKVJ.jpg?1",
+    description: "Adobo steak, blackened chicken, or carnitas",
+    allergies: ["Dairy"]
+  },
+  {
+    name: "Fusilli Bolognese",
+    img: "https://i.imgur.com/di711Oc.jpg?1",
+    description: "Hand rolled fusilli, beef & pork bolognese, ricotta cheese, 6min egg",
+    allergies: ["Dairy", "Gluten"]
+  },
+  {
+    name: "Roast + Frites",
+    img: "https://i.imgur.com/KTgZOEC.jpg",
+    description: "Medium rare roast served with herbed fries",
+    allergies: []
+  },
+  {
     name: "Shrimp Gumbo",
-    img: "https://i.imgur.com/f0NlFFg.jpg",
-    description: "Cajun rice, North Country smokehouse andouille, okra, dark roux.",
-    allergies: ["Shellfish"]
+    img: "https://i.imgur.com/f0NlFFg.jpg1",
+    description: "Cajun rice, north country smokehouse andouille, okra, dark roux",
+    allergies: ["Shellfish", "Gluten"]
   }
 ]
